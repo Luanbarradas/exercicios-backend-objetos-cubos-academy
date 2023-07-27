802659753f4ffc852de23bbffd18badd00ac52c1
Luan Barradas <barradas.luk@gmail.com>
2023-07-27 16:11:39 -0300

    resolução de 4 exercícios de 10

---
+++ 01/index.js
@@ -0,0 +1,19 @@
+const endereco ={
+    rua: "mundurucus",
+    numero: 123,
+    complemento:"baixo",
+    bairro:"batista campos",
+    cep: 66123456,
+    cidade:"belém",
+    estado:"pará",
+    pais:"brasil"
+}
+const usuario = {
+    nome:"joão",
+    email:"joão123@gmail.com",
+    telefone:91912345678,
+    data_nascimento:"1 de janeiro de 2000",
+    endereco
+}
+
+console.log(usuario)
---
+++ 02/index.js
@@ -0,0 +1,28 @@
+const carros = {
+carro1 : {
+    marca: "fiat",
+    modelo: "500",
+    ano: 2019,
+    cor: "vermelho",
+    quantidade_portas: 2,
+    automatico: true
+},
+carro2 : {
+    marca: "fiat",
+    modelo: "cronos drive",
+    ano: 2023,
+    cor: "preto",
+    quantidade_portas: 4,
+    automatico: false
+},
+carro3 : {
+    marca: "chevrolet",
+    modelo: "onix lt",
+    ano: 2018,
+    cor: "branco",
+    quantidade_portas: 4,
+    automatico: false
+}
+}
+
+console.log(carros)
