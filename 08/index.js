3986f5b2f6655454e6d5269c9e969c2448a8491b
Luan Barradas <barradas.luk@gmail.com>
2023-07-27 19:55:23 -0300

    Exercício de objetos finalizado

---
+++ 08/index.js
@@ -0,0 +1,39 @@
+//a. Se a pessoa não tiver pets, a mensagem deve ser: Sou João e não tenho pets.
+
+//b. Se a pessoa tiver mais de um pet, a mensagem deve ser: Sou Ana e tenho 2 pets.
+
+//c. Se a pessoa tiver um pet, a mensagem deve ser: Sou Beatriz e tenho 1 pet.
+
+
+const usuarios = [
+    {
+        nome: "João",
+        pets: [],
+    },
+    {
+        nome: "Ana",
+        pets: ["Pingo", "Lulu"],
+    },
+    {
+        nome: "Beatriz",
+        pets: ["Lessie"],
+    },
+    {
+        nome: "Carlos",
+        pets: ["Farofa", "Salsicha", "Batata"],
+    },
+    {
+        nome: "Antonio",
+        pets: ["Naninha"],
+    },
+]
+
+for (const {pets, nome} of usuarios) {
+    if (pets.length == 0) {
+        console.log(`Sou ${nome} e não tenho pets.`);
+    } else if (pets.length == 1) {
+        console.log(`Sou ${nome} e tenho 1 pet.`);
+    } else {
+        console.log(`Sou ${nome} e tenho ${pets.length} pets.`);
+    }
+}
